eb4b10bc6412f8cae0a5b9be1e95924af56eecf2
wnbrata <123700940+wnbrata@users.noreply.github.com>
2023-01-27 00:28:11 +0100

    Update ipfs-upload.js

--- ipfs-upload.js
+++ ipfs-upload.js
@@ -1,10 +1,10 @@
-async function uploadToIpfs() {
-    // Get the selected file from the input
-    const file = document.getElementById("fileInput").files[0];
-    // Create an IPFS instance
-    const ipfs = IpfsHttpClient({ host: 'ipfs.io', port: '443', protocol: 'https' })
-    // Add the file to IPFS
-    const addedFile = await ipfs.add(file);
-    console.log("IPFS Hash: "+addedFile[0].hash);
-    console.log("Blockchain link: https://ipfs.io/ipfs/"+addedFile[0].hash);
-}
+    async function uploadToIpfs() {
+        // Get the selected file from the input
+        const file = document.getElementById("fileInput").files[0];
+        // Create an IPFS instance
+        const ipfs = IpfsHttpClient({ host: 'ipfs.io', port: '443', protocol: 'https' })
+        // Add the file to IPFS
+        const addedFile = await ipfs.add(file);
+        console.log("IPFS Hash: "+addedFile[0].hash);
+        console.log("Blockchain link: https://ipfs.io/ipfs/"+addedFile[0].hash);
+      }
