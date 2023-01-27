c7650ab3f082e433a3400882386d5e119d0beb95
wnbrata <123700940+wnbrata@users.noreply.github.com>
2023-01-27 01:16:27 +0100

    Update ipfs-upload.js

--- ipfs-upload.js
+++ ipfs-upload.js
@@ -1,13 +1,15 @@
-<script>
-      async function uploadToIpfs() {
-        // Get the selected file from the input
-        const file = document.getElementById("fileInput").files[0];
-        // Create an IPFS instance
-        const ipfs = IpfsHttpClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
-        // Add the file to IPFS
-        const addedFile = await ipfs.add(file);
-        console.log("IPFS Hash: "+addedFile[0].hash);
-        console.log("Blockchain link: https://ipfs.infura.io/ipfs/"+addedFile[0].hash);
-        document.getElementById("consoleOutput").innerHTML = "IPFS Hash: "+addedFile[0].hash+"<br>Blockchain link: https://ipfs.infura.io/ipfs/"+addedFile[0].hash;
-      }
-    </script>
+const IPFS = require('ipfs-http-client');
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
+async function uploadToIpfs() {
+  const file = document.getElementById("file").files[0];
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onloadend = async () => {
+    const buffer = await Buffer.from(reader.result);
+    const ipfsHash = await ipfs.add(buffer);
+    const ipnsHash = await ipfs.name.publish(ipfsHash[0].hash);
+    document.getElementById("ipfsHash").innerHTML = "IPFS Hash: " + ipfsHash[0].hash;
+    document.getElementById("blockchainLink").innerHTML = "Blockchain Link: " + "https://ipfs.io/ipns/" + ipnsHash.name;
+  }
+}
