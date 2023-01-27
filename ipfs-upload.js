1002f216ddfad5cf465be3ddfda007e5e955f137
wnbrata <123700940+wnbrata@users.noreply.github.com>
2023-01-27 02:37:39 +0100

    Update ipfs-upload.js

--- ipfs-upload.js
+++ ipfs-upload.js
@@ -1,15 +1,12 @@
-const IPFS = require('ipfs-http-client');
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const IPFS = require('ipfs-http-client')
+const ipfs = new IPFS({ host: 'api.pinata.cloud', port: '443', protocol: 'https' }, { apiKey: 'db706ab6cd2f3b7aab6a' })
 
 async function uploadToIpfs() {
   const file = document.getElementById("file").files[0];
   const reader = new FileReader();
-  reader.readAsArrayBuffer(file);
-  reader.onloadend = async () => {
-    const buffer = await Buffer.from(reader.result);
-    const ipfsHash = await ipfs.add(buffer);
-    const ipnsHash = await ipfs.name.publish(ipfsHash[0].hash);
-    document.getElementById("ipfsHash").innerHTML = "IPFS Hash: " + ipfsHash[0].hash;
-    document.getElementById("blockchainLink").innerHTML = "Blockchain Link: " + "https://ipfs.io/ipns/" + ipnsHash.name;
-  }
-}
+
+  reader.onloadend = async function() {
+    const buffer = Buffer.from(reader.result);
+    const results = await ipfs.add(buffer);
+    const hash = results[0].hash;
+    document.getElementById("ipfs-link").innerHTML = `IPFS
